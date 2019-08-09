aa4c846b528c2903d1b1234f32d1008e485e0299
Alfian Losari <alfianlosari@gmail.com>
2019-08-09 09:59:27 +0700

    Initial Commit

---
+++ SwiftUI-GameDB/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI-GameDB
+//
+//  Created by Alfian Losari on 09/08/19.
+//  Copyright © 2019 Alfian Losari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
